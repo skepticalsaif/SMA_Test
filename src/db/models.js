0bc805ca190c8ed7978ac44a845ee52475ea05da
skepticalsaif <saifnizami.official@gmail.com>
2021-08-16 23:38:51 +0530

    Add pg package
    Signed-off-by: skepticalsaif <saifnizami.official@gmail.com>

--- src/db/models.js
+++ src/db/models.js
@@ -14,6 +14,7 @@ const Sequelize = require('sequelize')
 const db = new Sequelize({
   dialect: 'postgres',
   database: 'ddq3fd08q6f43',
+  port: 5432,
   username: 'iihapdfvhrxztt',
   password: 'c31ce091e661b2b70fe37c60d718ef233f7404af0ddab775ee160daf938cce15',
   host: 'ec2-54-236-234-167.compute-1.amazonaws.com',
